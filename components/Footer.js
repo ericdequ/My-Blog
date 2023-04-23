3e43dc910af3b6c3602ebbaa3ad0dc69a8c0598a
Eric deQuevedo <ericdequevedo@gmail.com>
2023-04-23 19:37:46 -0400

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,4 @@
-import Link from './Link'
+import Link from 'next/link'
 import siteMetadata from '@/data/siteMetadata'
 import SocialIcon from '@/components/social-icons'
 
@@ -20,11 +20,6 @@ export default function Footer() {
           <div>{`© ${new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
           <Link href="/">{siteMetadata.title}</Link>
-        </div>
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
-          </Link>
         </div>
       </div>
     </footer>
