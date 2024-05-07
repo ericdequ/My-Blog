7fa489cd9159b2d9405e07af3a575f6e5b01db6f
Eric <ericdequevedo@gmail.com>
2024-05-07 14:55:44 -0400

    Author Layout no links

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion'
 import SocialIcon from '@/components/social-icons'
 import Image from '@/components/Image'
 import { PageSEO } from '@/components/SEO'
@@ -5,17 +6,38 @@ import { PageSEO } from '@/components/SEO'
 export default function AuthorLayout({ children, frontMatter }) {
   const { name, avatar, occupation, company, email, twitter, linkedin, github } = frontMatter
 
+  const variants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+  }
+
   return (
     <>
       <PageSEO title={`About - ${name}`} description={`About me - ${name}`} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+        <motion.div
+          className="space-y-2 pt-6 pb-8 md:space-y-5"
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: { opacity: 0, y: 20 },
+            visible: { opacity: 1, y: 0, transition: { duration: 0.5, delay: 0.2 } },
+          }}
+        >
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             About
           </h1>
-        </div>
+        </motion.div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
-          <div className="flex flex-col items-center pt-8">
+          <motion.div
+            className="flex flex-col items-center pt-8"
+            initial="hidden"
+            animate="visible"
+            variants={{
+              hidden: { opacity: 0, y: 20 },
+              visible: { opacity: 1, y: 0, transition: { duration: 0.5, delay: 0.4 } },
+            }}
+          >
             <Image
               src={avatar}
               alt="avatar"
@@ -32,8 +54,18 @@ export default function AuthorLayout({ children, frontMatter }) {
               <SocialIcon kind="linkedin" href={linkedin} />
               <SocialIcon kind="twitter" href={twitter} />
             </div>
-          </div>
-          <div className="prose max-w-none pt-8 pb-8 dark:prose-dark xl:col-span-2">{children}</div>
+          </motion.div>
+          <motion.div
+            className="prose max-w-none pt-8 pb-8 dark:prose-dark xl:col-span-2"
+            initial="hidden"
+            animate="visible"
+            variants={{
+              hidden: { opacity: 0, y: 20 },
+              visible: { opacity: 1, y: 0, transition: { duration: 0.5, delay: 0.6 } },
+            }}
+          >
+            {children}
+          </motion.div>
         </div>
       </div>
     </>
