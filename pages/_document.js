46f127ca23b845090a2eeb955d261dbfafe0f157
Eric <ericdequevedo@gmail.com>
2024-05-06 22:45:29 -0400

    document

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,9 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en" className="scroll-smooth">
+      <Html lang="en">
         <Head>
           <link rel="manifest" href="/manifest.json" />
           <link rel="icon" sizes="76x76" href="/static/favicons/apple-touch-icon.webp" />
@@ -10,35 +11,21 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://www.quantumcybersolutions.com/" />
           <link rel="dns-prefetch" href="https://www.quantumcybersolutions.com/" />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/static/favicons/favicon-32x32.webp"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/static/favicons/favicon-16x16.webp"
-          />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.webp" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.webp" />
           <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#000000" />
-          <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
-          <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
+          <meta name="theme-color" content="#ffffff" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-          <meta
-            name="google-site-verification"
-            content="At3RZ4YGQtXrtPbI2UKNk1aKwGJ5P7goMttGiLMTaT4"
-          />
+          <meta name="google-site-verification" content="At3RZ4YGQtXrtPbI2UKNk1aKwGJ5P7goMttGiLMTaT4" />
         </Head>
-        <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
+        <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
